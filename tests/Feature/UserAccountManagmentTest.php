88a4041132c4cdc9b3a6d484688d312b1e815a85
Asma_AH <a.hawari@zkood.com>
2021-12-07 22:59:49 +0200

    add Negative test

--- tests/Feature/UserAccountManagmentTest.php
+++ tests/Feature/UserAccountManagmentTest.php
@@ -8,10 +8,12 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Mail\Transport\SesTransport;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Product;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class UserAccountManagmentTest extends TestCase
 {
@@ -39,15 +41,19 @@ class UserAccountManagmentTest extends TestCase
 
     /**
      * test get all products.
+     * Positive Test
      *
      * @return void
      */
     public function test_get_user_data()
     {
         $token = $this->authenticate();
+
         $response = $this->withHeaders([
             'Authorization' => 'Bearer ' . $token,
         ])->json('GET', 'api/user');
+
+
         //Write the response in laravel.log
         \Log::info(1, [$response->getContent()]);
         $response->assertStatus(200);
@@ -58,7 +64,26 @@ class UserAccountManagmentTest extends TestCase
 
     /**
      * test get all products.
+     * Negative Test "Unauthenticated"
      *
+     * @return void
+     */
+    public function test_get_user_data_Unauthenticated()
+    {
+        $token = Str::random(32);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('GET', 'api/user');
+
+        //Write the response in laravel.log
+        \Log::info(1, [$response->getContent()]);
+        $response->assertStatus(401);
+    }
+
+    /**
+     * test get all products.
+     *Positive Test
      * @return void
      */
     public function test_get_user_products()
@@ -74,10 +99,38 @@ class UserAccountManagmentTest extends TestCase
 
         $response->assertStatus(200);
     }
+    /**
+     * test get all products.
+     *Negative Test
+     * @return void
+     */
+    public function test_get_user_products_Unauthenticated()
+    {
+        $token = Str::random(32);
 
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('GET', 'api/user/products');
+
+        //Write the response in laravel.log
+        \Log::info(1, [$response->getContent()]);
+
+        $response->assertStatus(401);
+    }
+
+    public function getSkus()
+    {
+         return DB::table('products')
+            ->select('sku')
+            ->get()->toArray();
+    }
+
+    /**
+     * @return bool
+     */
     public function getProductSKU()
     {
-        $product = Product::find(1)->first();
+        $product = Product::all()->random();
         if ($product) {
             return $product->sku;
         } else
@@ -86,22 +139,16 @@ class UserAccountManagmentTest extends TestCase
 
     /**
      * test create product.
-     *
+     * Positive Test
      * @return void
      */
     public function test_get_purchased_item()
     {
-        $sku = $this->getProductSKU();
-
-        $this->assertIsString($sku);
-
         $token = $this->authenticate();
 
         $response = $this->withHeaders([
             'Authorization' => 'Bearer ' . $token,
-        ])->json('GET', 'api/user/products', [
-            'sku' => $sku,
-        ]);
+        ])->json('GET', 'api/user/products');
 
         //Write the response in laravel.log
         \Log::info(1, [$response->getContent()]);
@@ -110,15 +157,35 @@ class UserAccountManagmentTest extends TestCase
     }
 
     /**
+     * test create product.
+     * Negative test
+     * @return void
+     */
+    public function test_get_purchased_item_Unauthenticated()
+    {
+        $token = Str::random(32);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('GET', 'api/user/products');
+
+        //Write the response in laravel.log
+        \Log::info(1, [$response->getContent()]);
+
+        $response->assertStatus(401);
+    }
+
+    /**
      * test update product.
-     *
+     * Positive Test
      * @return void
      */
     public function test_add_purchased_item()
     {
-        $sku = $this->getProductSKU();
 
-        $this->assertIsString($sku);
+        $sku = $this->getSkus()[0];
+
+        $this->assertIsString($sku->sku);
 
         $token = $this->authenticate();
 
@@ -133,10 +200,61 @@ class UserAccountManagmentTest extends TestCase
 
         $response->assertStatus(200);
     }
+    /**
+     * test update product.
+     * Negative Test - Unauthorized
+     * @return void
+     */
+    public function test_add_purchased_item_unauthorized()
+    {
+
+        $sku = $this->getSkus()[0];
+
+        $this->assertIsString($sku->sku);
+
+        $token = STR::random(32);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('POST', 'api/user/products', [
+            'sku' => $sku,
+        ]);
+
+        //Write the response in laravel.log
+        \Log::info(1, [$response->getContent()]);
+
+        $response->assertStatus(401);
+    }
+
+    /**
+     * test update product.
+     * Negative Test - Not Found
+     * @return void
+     */
+    public function test_add_purchased_item_not_found()
+    {
+
+        $sku = STR::random(3);
+
+        $this->assertIsString($sku);
+
+        $token = $this->authenticate();
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('POST', 'api/user/products', [
+            'sku' => $sku,
+        ]);
+
+        //Write the response in laravel.log
+        \Log::info(1, [$response->getContent()]);
+
+        $response->assertStatus(404);
+    }
 
     /**
      * test delete products.
-     *
+     * Positive Test
      * @return void
      */
     public function test_delete_purchased_item()
@@ -155,6 +273,49 @@ class UserAccountManagmentTest extends TestCase
         \Log::info(1, [$response->getContent()]);
 
         $response->assertStatus(200);
+    }
+    /**
+     * test delete products.
+     * Negative Test - unauthorized
+     * @return void
+     */
+    public function test_delete_purchased_item_unauthorized()
+    {
+        $sku = $this->getProductSKU();
+
+        $this->assertIsString($sku);
+
+        $token = Str::random(32);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('DELETE', 'api/user/products/' . $sku);
+
+        //Write the response in laravel.log
+        \Log::info(1, [$response->getContent()]);
+
+        $response->assertStatus(401);
+    }
+    
+    /**
+     * test delete products.
+     * Negative Test - Not Found
+     * @return void
+     */
+    public function test_delete_purchased_item_not_found()
+    {
+        $sku = STR::random(4);
+
+        $token = $this->authenticate();
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('DELETE', 'api/user/products/' . $sku);
+
+        //Write the response in laravel.log
+        \Log::info(1, [$response->getContent()]);
+
+        $response->assertStatus(404);
     }
 
 
